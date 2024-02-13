10e8523d172939feeee3b7e3fee88084b84500e2
Mouamar6 <mouamar.adjaho@epitech.eu>
2024-02-13 08:36:19 +0100

    rm useless function

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
-import ContactForm from './screens/ContactForm';
-import Projets from './screens/Projets';
-import Rocket from './screens/Rocket';
-import Hub from './screens/Hub';
+// import ContactForm from './screens/ContactForm';
+// import Projets from './screens/Projets';
+// import Rocket from './screens/Rocket';
+// import Hub from './screens/Hub';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Router>
+      {/* <Router>
           <Routes>
             <Route path="/" element={<Rocket />} />
           </Routes>
-      </Router>
+      </Router> */}
     </>
   );
 };
